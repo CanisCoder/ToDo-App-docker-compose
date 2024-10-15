1c31cc4dab0a4e97e0868e8a1a806d658952cd50
Anand Kumar Maurya <anand@anand.Kumar>
2024-10-15 17:31:00 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const tasksRoute = require("./routes/tasks");
+
+mongoose.set('strictQuery', false);
+
+mongoose
+  .connect("mongodb://mongo-db/ToDoAppDb")
+  .then(() => console.log("Connected"))
+  .catch(() => console.log("Not connected"));
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("combined"));
+
+app.use(tasksRoute);
+
+app.listen(5000, console.log("Running on 5000"));
